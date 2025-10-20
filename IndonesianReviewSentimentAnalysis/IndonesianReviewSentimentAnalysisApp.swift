8345b780471263db0026a701328dc28acbd8097b
far1h <farihmuhammad45@gmail.com>
2025-10-20 19:37:28 +0700

    Initial Commit

---
+++ IndonesianReviewSentimentAnalysis/IndonesianReviewSentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndonesianReviewSentimentAnalysisApp.swift
+//  IndonesianReviewSentimentAnalysis
+//
+//  Created by Farih Muhammad on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct IndonesianReviewSentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
